9897ab01e4183368e03d2e09abb1591d39a6ffd9
1Royal <royalmarinellp@gmail.com>
2024-10-09 09:26:47 +0530

    9 Oct

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,56 @@
+import p_1 from "./p_1.jpeg";
+import p_2 from "./p_2.jpeg";
+import p_3 from "./p_3.jpeg";
+import p_4 from "./p_4.jpeg";
+import p_5 from "./p_5.jpeg";
+import p_6 from "./p_6.jpeg";
+import p_7 from "./p_7.jpeg";
+import p_8 from "./p_8.jpeg";
+import p_9 from "./p_9.jpeg";
+import p_10 from "./p_10.jpeg";
+import p_11 from "./p_11.jpeg";
+import p_12 from "./p_12.jpeg";
+
+// import logo from "./logo.png";
+// import hero_img from "./hero-img.png";
+import cart_icon from "./cart-icon.svg";
+import bin_icon from "./bin-icon.svg";
+import contact_icon from "./contact-call-icon.svg";
+import cross_icon from "./cross-icon.svg";
+import menu_bar_icon from "./menu-bar-icon.svg";
+import profile_icon from "./profile-icon.svg";
+import support_icon from "./support-icon.svg";
+import search_icon from "./search-icon.svg";
+
+export const assets = {
+  search_icon,
+  // logo,
+  // hero_img,
+  cart_icon,
+  bin_icon,
+  contact_icon,
+  cross_icon,
+  menu_bar_icon,
+  profile_icon,
+  support_icon,
+  p_1,
+  p_2,
+  p_3,
+  p_4,
+  p_5,
+  p_6,
+  p_7,
+  p_8,
+  p_9,
+  p_10,
+  p_11,
+  p_12,
+};
+
+export const products = [
+  { id: 1, name: "Morly", description: "Nevin", images: [p_1, p_2, p_7, p_8], "date":"9/29/2024" },
+  { id: 2, name: "Emmanuel", description: "Lanny", images: [p_1, p_2, p_3, p_9], "date":"8/6/2024" },
+  { id: 3, name: "Edmon", description: "Judd", images: [p_1, p_2, p_4, p_10], "date":"10/16/2023" },
+  { id: 4, name: "Lars", description: "Gustavo", images: [p_1, p_2, p_5, p_11], "date":"1/21/2024" },
+  { id: 5, name: "Burr", description: "Jerry", images: [p_1, p_2, p_6, p_12], "date":"10/9/2023" },
+];
